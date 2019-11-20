5358af1107dff0f57625c3f38ccc5ead84063831
Chenyuxi <Chenyuxicc@users.noreply.github.com>
2019-11-20 11:27:38 +0800

    update

--- exercise/src/com/chen/leetcode/arrrayMath/S35_searchInsert.java
+++ exercise/src/com/chen/leetcode/arrrayMath/S35_searchInsert.java
@@ -6,10 +6,12 @@ package com.chen.leetcode.arrrayMath;
  * @since 2019/11/19 on 21:46
  **/
 public class S35_searchInsert {
+    static int anInt = 0;
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         int mid = left;
+        anInt++;
         while (left <= right){
             mid = (left + right) / 2;
             if(nums[mid] == target){
@@ -20,6 +22,9 @@ public class S35_searchInsert {
                 right = mid - 1;
             }
         }
+        //left是最终为值，但right是最终值-1
+        System.out.println(left);
+        System.out.println(right);
         if(nums[mid] == target){
             return mid;
         }else {
@@ -29,6 +34,6 @@ public class S35_searchInsert {
 
     public static void main(String[] args) {
         int[] nums = {1,3,5,6};
-        System.out.println(searchInsert(nums,7));
+        System.out.println(searchInsert(nums,3));
     }
 }
